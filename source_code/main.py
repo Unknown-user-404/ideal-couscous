a71ceab16d97274c48aef07da02a6c3b42725aaa
Unknown-user-404 <100158628+Unknown-user-404@users.noreply.github.com>
2022-11-29 13:49:28 -0500

    Updated script to make it clean
    yay

--- source_code/main.py
+++ source_code/main.py
@@ -1,9 +1,9 @@
 from PIL import ImageDraw,ImageFont,Image
+from audio import grab_audio, insert_audio
 import cv2
 import numpy as np
 import math
 import os
-import glob
 
 cwd = os.getcwd()
 try:
@@ -16,6 +16,13 @@ fileName=input("Type in the video name in the input folder(if no .mp4 video plea
 if not '.' in fileName:
     print("Please include file extension")
     exit()
+found = False
+for i in os.listdir('./'):
+    if fileName == i:
+        found = True
+if not found:
+    print(f"No file called {fileName} found")
+    exit()
 chars = " .'`^\",:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 charlist=list(chars)
 charlen=len(charlist)
@@ -24,9 +31,35 @@ scale_factor=0.09
 charwidth=10
 charheight=10
 while True:
-    fps=input("Type in the fps(frames per second) you want in the result video(will impact the time of the video): ")
-    if fps.isdecimal():
+    try:
+        print("Type in the fps(frames per second) you want in the result video(will impact the time of the video)")
+        print("Press ctrl+c to use the origonal video fps")
+        fps=input(": ")
+        if fps.isdecimal():
+            break
+    except KeyboardInterrupt:
+        fps="ORIGINAL"
         break
+
+print("")
+
+try:
+    video_type=input("Please choose a video type(enter for avi, ctrl+c for mp4)\n")
+    if video_type == "":
+        video_type = "avi"
+except KeyboardInterrupt:
+    video_type = "mp4"
+
+while True:
+    start_frame=input("Enter a start frame if you want to skip frames, leave blank for no: ")
+    if start_frame == "":
+        start_frame = None
+        break
+    elif start_frame.isdecimal():
+        start_frame = int(start_frame)
+        break
+    else:
+        print("Wrong value, please try again")
 
 def get_char(i):
     return charlist[math.floor(i*interval)]
@@ -36,8 +69,48 @@ try:
 except AttributeError:
     print(f"File {fileName} does not exist(please include file extension)")
 
+if fps == "ORIGINAL":
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+def skip_frame_create_vid(amount: int, frames_count: int):
+    numb = 0 - 1
+    Font=ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf',15)
+    while True:
+        try:
+            numb += 1
+            if not numb < amount:
+                _, img=cap.read()
+                img=Image.fromarray(img)
+
+                width,height=img.size
+                img=img.resize((int(scale_factor*width),int(scale_factor*height*(charwidth/charheight))),Image.Resampling.NEAREST)
+                width,height=img.size
+                pixel=img.load()
+                outputImage=Image.new("RGB",(charwidth*width,charheight*height),color=(0,0,0))
+                dest=ImageDraw.Draw(outputImage)
+
+                for i in range(height):
+                    for j in range(width):
+                        r,g,b=pixel[j,i]
+                        h=int(0.299*r+0.587*g+0.114*b)
+                        pixel[j,i]=(h,h,h)
+                        dest.text((j*charwidth,i*charheight),get_char(h),font=Font,fill=(r,g,b))
+
+                open_cv_image=np.array(outputImage)
+                key=cv2.waitKey(1)
+                if key == ord("q"):
+                    break
+                print(f"Current Frame: {numb} | {frames_count}", end="\r")
+                cv2.imwrite(f"{numb}.png",open_cv_image)
+        except AttributeError:
+            break
+
+if not start_frame is None:
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+frames_count = math.floor(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 Font=ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf',15)
 numb = 0 - 1
+frame_num = -1
 os.chdir(cwd)
 try:
     os.chdir('./frames')
@@ -46,40 +119,45 @@ except FileNotFoundError:
     os.chdir('./frames')
 print("Generating frames(This could took a wile)...")
 print("Current Frame: ", end="\r")
-while True:
-    try:
-        numb += 1
-        _, img=cap.read()
-        img=Image.fromarray(img)
+if start_frame is None:
+    while True:
+        try:
+            numb += 1
+            _, img=cap.read()
+            img=Image.fromarray(img)
 
-        width,height=img.size
-        img=img.resize((int(scale_factor*width),int(scale_factor*height*(charwidth/charheight))),Image.Resampling.NEAREST)
-        width,height=img.size
-        pixel=img.load()
-        outputImage=Image.new("RGB",(charwidth*width,charheight*height),color=(0,0,0))
-        dest=ImageDraw.Draw(outputImage)
+            width,height=img.size
+            img=img.resize((int(scale_factor*width),int(scale_factor*height*(charwidth/charheight))),Image.Resampling.NEAREST)
+            width,height=img.size
+            pixel=img.load()
+            outputImage=Image.new("RGB",(charwidth*width,charheight*height),color=(0,0,0))
+            dest=ImageDraw.Draw(outputImage)
 
-        for i in range(height):
-            for j in range(width):
-                r,g,b=pixel[j,i]
-                h=int(0.299*r+0.587*g+0.114*b)
-                pixel[j,i]=(h,h,h)
-                dest.text((j*charwidth,i*charheight),get_char(h),font=Font,fill=(r,g,b))
+            for i in range(height):
+                for j in range(width):
+                    r,g,b=pixel[j,i]
+                    h=int(0.299*r+0.587*g+0.114*b)
+                    pixel[j,i]=(h,h,h)
+                    dest.text((j*charwidth,i*charheight),get_char(h),font=Font,fill=(r,g,b))
 
-        open_cv_image=np.array(outputImage)
-        key=cv2.waitKey(1)
-        if key == ord("q"):
+            open_cv_image=np.array(outputImage)
+            key=cv2.waitKey(1)
+            if key == ord("q"):
+                break
+            print(f"Current Frame: {numb} | {frames_count - 1}", end="\r")
+            cv2.imwrite(f"{numb}.png",open_cv_image)
+            frame_num += 1
+        except AttributeError:
             break
-        print(f"Current Frame: {numb}", end="\r")
-        cv2.imwrite(f"{numb}.png",open_cv_image)
-    except AttributeError:
-        break
-print("Done creating frames")
+else:
+    skip_frame_create_vid(start_frame, frames_count)
+
+print(f"Done creating frames, total count {frame_num}")
 cap.release()
 
 os.chdir(cwd)
 
-print("Making vid...")
+print(f"Making vid... | {video_type}")
 img_array = []
 for i in range(len(os.listdir('./frames'))):
     img = cv2.imread(f"./frames/{i}.png")
@@ -90,7 +168,10 @@ for i in range(len(os.listdir('./frames'))):
 
 print("Done fetching frames")
 size = size_
-out = cv2.VideoWriter('result.avi', cv2.VideoWriter_fourcc(*'DIVX'), int(fps), size_)
+if video_type == "avi":
+    out = cv2.VideoWriter('result.avi', cv2.VideoWriter_fourcc(*'DIVX'), int(fps), size_)
+elif video_type == "mp4":
+    out = cv2.VideoWriter('result.mp4', cv2.VideoWriter_fourcc(*'mp4v'), int(fps), size_)
 print("compiling...")
 
 for i in range(len(img_array)):
@@ -98,3 +179,19 @@ for i in range(len(img_array)):
     print(f'compiling frame {i} | {len(img_array)}', end="\r")
 print("Done creating ascii video")
 out.release()
+nfile = -1
+os.chdir('./frames')
+for file in os.listdir('./'):
+    nfile += 1
+    os.remove(str(nfile) + '.png')
+os.chdir(cwd)
+grab_audio(fileName)
+print("Audio generated")
+os.rmdir('./frames')
+try:
+    input("Press enter to exit, or press ctrl+c to insert the audio into the result video")
+except KeyboardInterrupt:
+    if video_type == "avi":
+        insert_audio('.avi')
+    elif video_type == "mp4":
+        insert_audio('.mp4')
